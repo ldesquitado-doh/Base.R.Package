554b1346625a42056a09ba4e422c22811e541d59
ldesquitado-doh <ledesquitado@doh.gov.ph>
2025-02-05 15:15:19 +0800

    Created package and test function.

--- R/test_function.R
+++ R/test_function.R
@@ -1,3 +1,8 @@
+#' Test function that logs a string into the console.
+#'
+#' @param input_str String to be logged into R console.
+#'
+#' @export
 test_function <- function(input_str) {
   print(input_str)
 }
